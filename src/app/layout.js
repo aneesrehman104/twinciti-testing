8a2b277c2a15d6789ae006c20fa05da2361083ad
Anees ur Rehman <anees.rehman@zweidevs.com>
2024-04-16 17:45:21 +0500

    build issue resolved

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { Suspense } from 'react';
 
 import './globals.css';
 
@@ -14,7 +15,9 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <AntdRegistry>{children}</AntdRegistry>
+                <Suspense>
+                    <AntdRegistry>{children}</AntdRegistry>
+                </Suspense>
             </body>
         </html>
     );
