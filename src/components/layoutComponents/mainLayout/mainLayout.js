3ae91a26e8a42916b06e147fbb16258f761ccb72
Anees ur Rehman <anees.rehman@zweidevs.com>
2024-04-19 10:48:29 +0500

    hidration issue fixed

--- src/components/layoutComponents/mainLayout/mainLayout.js
+++ src/components/layoutComponents/mainLayout/mainLayout.js
@@ -1,8 +1,9 @@
 'use client';
+import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
 import { Layout } from 'antd';
 import SiderComponent from '../sidebarLayout/sidebarLayout';
-import NavbarLayout from '../navbarLayout/navbarLayout';
+// import NavbarLayout from '../navbarLayout/navbarLayout';
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import ModalSidebar from '../../sidebarComponents/modelsSidebar/modelsSidebar';
 import DiscoverSidebar from '../../sidebarComponents/discoverSidebar/discoverSidebar';
@@ -12,7 +13,9 @@ import styles from './mainLayoutStyle.module.css';
 const ContentLayout = dynamic(() =>
     import('../../layoutComponents/contentLayout/contentLayout'),
 );
-
+const NavbarLayout = dynamic(() => import('../navbarLayout/navbarLayout'), {
+    ssr: false,
+});
 const layoutStyle = {
     overflow: 'hidden',
     width: '100%',
